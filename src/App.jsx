af5a24bcff92c3f365560ce9df65539ae586a2e7
Luciano <luchosr@gmail.com>
2019-07-23 17:43:33 -0300

    add transactions history page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import Login from "./components/login/Login";
-import AuthenticationWrapper from './components/common/AuthenticationWrapper';
-import DashboardContainer from './containers/DashboardContainer';
+import AuthenticationWrapper from "./components/common/AuthenticationWrapper";
+import DashboardContainer from "./containers/DashboardContainer";
+import UserTransactions from "./components/transactions/UserTransactions";
 
 function App() {
+  const baseURL =
+    process.env.NODE_ENV === "production"
+      ? "https://veinte.net/api"
+      : "https://veinte.net/api";
 
-  const baseURL = process.env.NODE_ENV === 'production' ? 'https://veinte.net/api' : 'https://veinte.net/api';
-  
   window.APIS = {
-    LOGIN: baseURL + '/login',
-    REGISTER: baseURL + '/register',
+    LOGIN: baseURL + "/login",
+    REGISTER: baseURL + "/register"
   };
 
-  const checkAuth = (routerProps, component, needsAuthentication) => (
+  const checkAuth = (routerProps, component, needsAuthentication) =>
     AuthenticationWrapper({
       routerProps,
       component,
       needsAuthentication
-    })
-  );
+    });
 
-  const WrappedLogin = (props) => checkAuth(props, <Login {...props} />, false);
-  const WrappedDashboard = (props) => checkAuth(props, <DashboardContainer {...props} />, true);
+  const WrappedLogin = props => checkAuth(props, <Login {...props} />, false);
+  const WrappedDashboard = props =>
+    checkAuth(props, <DashboardContainer {...props} />, true);
 
   return (
     <BrowserRouter>
       <Redirect from="/" to="/app" />
-    <Route path="/app/" name="login" exact component={WrappedLogin} />
+      <Route path="/app/" name="login" exact component={WrappedLogin} />
       <Route path="/app/dashboard/" name="dash" component={WrappedDashboard} />
+      <Route
+        path="/app/transactions"
+        name="transactions"
+        component={UserTransactions}
+      />
     </BrowserRouter>
   );
 }
