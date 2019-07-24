71153615f7d4c3ccc97be30204d10d0d98e78d5c
Ramiro Martinez <ramiro.daniel.ing@gmail.com>
2019-07-24 17:20:03 -0300

    basic component for reference

--- src/components/admin/AdminDash.jsx
+++ src/components/admin/AdminDash.jsx
@@ -1,10 +1,80 @@
 import React from 'react';
+import { useState, useEffect } from "react";
+// import axios from 'axios';
+import { Container } from '@material-ui/core/';
+import Alert from '../common/Alert';
+import AdminDashList from './AdminDashList';
+import AdminActions from './AdminActions';
+import AdminDetails from './AdminDetails';
 
-export default function AdminDash(props) {
+function createData(user, tipo, metodo, moneda, cantidad, estado, operador, fecha) {
+  return { user, tipo, metodo, moneda, cantidad, estado, operador, fecha };
+}
+
+const rows = [
+  createData('Admin', 'tipo1', 'Metodo1', 'Moneda1', 10, 'Pendiente', 'Operador1', '10/06/2019'),
+  createData('User1', 'tipo1', 'Metodo1', 'Moneda4', 4, 'Aprobada', 'Operador2', '07/02/2019'),
+  createData('User2', 'tipo2', 'Metodo4', 'Moneda2', 2, 'Rechazada', 'Operador3', '14/07/2019'),
+  createData('User3', 'tipo4', 'Metodo3', 'Moneda3', 19, 'Pendiente', 'Operador4', '1/03/2019'),
+];
+
+export default function AdminDash() {
+
+  const [data, setData] = useState({ transactions: [] });
+  const [IsLoading, setIsLoading] = useState(true);
+
+  // FIXME: make alert component reusable 
+  const alertInitialState = {
+    show: false,
+    msg: {
+      title: '', description: ''
+    }
+  };
+  const [alert, setAlert] = useState(alertInitialState);
+
+  useEffect(() => {
+    // component did mount
+    getTransaction();
+  });
+
+  const getTransaction = () => {
+    try {
+      setData({ transactions: rows });
+      console.log('transactions', data.transactions);
+      setIsLoading(false);
+      // FIXME: implement api get
+      // const transactions = axios.get(window.API.TRANSACTION)
+    } catch (error) {
+      setAlert({
+        show: true,
+        msg: { title: 'Error in getTransaction.', description: 'Error description.' }
+      });
+      if (error.response) {
+        console.log('error in getTransaction', error.response.data);
+      } else {
+        console.log('error in getTransaction', error);
+      }
+    }
+  };
+
+  // Admindetails state/function
+  const [Detail, setDetail] = useState();
+
+  const [openDetails, setOpenDetails] = useState(false);
+
+  const handleDetails = (open, detail) => {
+    console.log('param', open ,detail);
+    
+    setDetail(detail);
+    setOpenDetails(open);
+  }
 
   return (
-    <div>
-      Admin Dash
-    </div>
+    <Container>
+      <AdminActions></AdminActions>
+      <AdminDetails open={openDetails} detail={Detail} setOpen={handleDetails}></AdminDetails>
+      <Alert show={alert.show} msg={alert.msg} handleClose={() => setAlert({ ...alertInitialState })}></Alert>
+      <AdminDashList isLoading={IsLoading} transactions={data.transactions} openDetails={handleDetails}></AdminDashList>
+    </Container>
   );
 }
--- src/containers/DashboardContainer.jsx
+++ src/containers/DashboardContainer.jsx
@@ -3,7 +3,7 @@ import UserDash from "../components/user/UserDash";
 import AdminDash from "../components/admin/AdminDash";
 
 export default function DashboardContainer(props) {
-  if (localStorage.getItem('role') !== 'admin') {
+  if (localStorage.getItem('role') === 'admin') {
     return <UserDash {...props}/>;
   }
   return <AdminDash {...props}/>;
