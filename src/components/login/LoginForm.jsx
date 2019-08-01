31436263504e855fa8839be2d84066ce7d00a5a4
Luciano <luchosr@gmail.com>
2019-08-01 10:52:30 -0300

    fix login validation issue

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,103 +1,214 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
 import {
-    Button,
-    TextField,
-    Checkbox,
-    FormControlLabel
+  Button,
+  TextField,
+  Checkbox,
+  FormControlLabel
 } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
+import { Formik } from "formik";
+import * as EmailValidator from "email-validator";
+import * as Yup from "yup";
+import "./validation.css";
 
 const useStyles = makeStyles(theme => ({
-    '@global': {
-        body: {
-            backgroundColor: theme.palette.common.white,
-        },
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 
 export default function LoginForm(props) {
-
-    const classes = useStyles();
-    const [loginForm, setLoginForm] = useState({
-        username: {
-            value: '',
-            error: false
-        },
-        password: {
-            value: '',
-            error: false
-        }
-    });
-
-    const handleInputChange = (event) => {
-        event.persist(); // https://fb.me/react-event-pooling
-        const newValue = {
-            value: event.target.value,
-            error: false, // validate
-        };
-        setLoginForm(loginForm => ({
-            ...loginForm,
-            [event.target.name]: newValue
-        }));
+  const classes = useStyles();
+  const [loginForm, setLoginForm] = useState({
+    email: {
+      value: "",
+      error: false
+    },
+    password: {
+      value: "",
+      error: false
     }
+  });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [mailValidation, setMailValidation] = useState(false);
 
-    const handleOnClickButton = () => {
-        props.onChange(loginForm);
-    }
+  const handleInputChange = event => {
+    event.persist(); // https://fb.me/react-event-pooling
+    const newValue = {
+      value: event.target.value,
+      error: validateEmail(event.target.value) // validate
+    };
+    setLoginForm(loginForm => ({
+      ...loginForm,
+      [event.target.name]: newValue
+    }));
+  };
 
-    return (
-        <div>
+  const handleOnClickButton = () => {
+    props.onChange(loginForm);
+  };
+  const handleEmail = event => {
+    setEmail(event.target.value);
+    setMailValidation(validateEmail(event.target.value));
+
+    console.log(validateEmail(event.target.value));
+  };
+  const handlePassword = event => {
+    setPassword(event.target.value);
+    validatePassword(event.target.value);
+  };
+
+  const validateEmail = email => {
+    const validEmailRegex = RegExp(
+      /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    );
+    return validEmailRegex.test(String(email).toLowerCase());
+  };
+  const validatePassword = password => {
+    const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
+    return regex.test(String(password));
+  };
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log("Submiting");
+      }}
+      //   validate={values => {
+      //     let errors = {};
+      //     if (!values.email) {
+      //       errors.email = "Required";
+      //     } else if (!EmailValidator.validate(values.email)) {
+      //       errors.email = "Invalid Email Address";
+      //     }
+      //     const passwordRegex = /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])/;
+      //     if (!values.password) {
+      //       errors.password = "Required";
+      //     } else if (values.password.length < 8) {
+      //       errors.password = "Password must be at least 8 characters long.";
+      //     } else if (!passwordRegex.test(values.password)) {
+      //       errors.password =
+      //         "Invalid password. It must contain at least one number, one upper case letter and one lower case letter";
+      //     }
+      //     return errors;
+      //   }}
+      //********Handling validation messages yourself*******/
+      // validate={values => {
+      //   let errors = {};
+      //   if (!values.email) {
+      //     errors.email = "Required";
+      //   } else if (!EmailValidator.validate(values.email)) {
+      //     errors.email = "Invalid email address";
+      //   }
+
+      //   const passwordRegex = /(?=.*[0-9])/;
+      //   if (!values.password) {
+      //     errors.password = "Required";
+      //   } else if (values.password.length < 8) {
+      //     errors.password = "Password must be 8 characters long.";
+      //   } else if (!passwordRegex.test(values.password)) {
+      //     errors.password = "Invalida password. Must contain one number";
+      //   }
+
+      //   return errors;
+      // }}
+      //********Using Yum for validation********/
+
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email()
+          .required("Required"),
+        password: Yup.string()
+          .required("No password provided.")
+          .min(8, "Password is too short - should be 8 chars minimum.")
+          .matches(
+            /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])/,
+            "Password must at least contain a number, an upper case letter and a lower case letter."
+          )
+      })}
+    >
+      {props => {
+        const {
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit
+        } = props;
+        return (
+          <form onSubmit={handleSubmit}>
             <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                autoComplete="username"
-                autoFocus
-                error={loginForm.username.error}
-                id="username"
-                name="username"
-                label="Username"
-                onChange={handleInputChange}
-                value={loginForm.username.value}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              autoComplete="email"
+              autoFocus
+              // error={!mailValidation}
+              id="email"
+              name="email"
+              label="Email"
+              onChange={handleChange}
+              onblur={handleBlur}
+              value={values.email}
+              className={errors.email && touched.email && "error"}
             />
+
+            {errors.email && touched.email && (
+              <div className="input-feedback">{errors.email}</div>
+            )}
             <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                error={loginForm.password.error}
-                onChange={handleInputChange}
-                value={loginForm.password.value}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              //error={validatePassword}
+              className={errors.email && touched.email && "error"}
+              onChange={handleChange}
+              onblur={handleBlur}
+              value={values.password}
             />
+            {errors.password && touched.password && (
+              <div className="input-feedback">{errors.password}</div>
+            )}
+
             <FormControlLabel
-                // TODO implement
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
+              // TODO implement
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
             />
             <Button
-                type="button"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-                onClick={handleOnClickButton}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              disabled={isSubmitting}
+              className={classes.submit}
+              //   onClick={handleOnClickButton}
             >
-                Sign In
+              Sign In
             </Button>
-        </div>
-    );
+          </form>
+        );
+      }}
+    </Formik>
+  );
 }
--- src/components/login/RegisterForm.jsx
+++ src/components/login/RegisterForm.jsx
@@ -1,140 +1,139 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
 import {
-    Button,
-    Grid,
-    FormControlLabel,
-    Checkbox,
-    TextField
+  Button,
+  Grid,
+  FormControlLabel,
+  Checkbox,
+  TextField
 } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-    '@global': {
-        body: {
-            backgroundColor: theme.palette.common.white,
-        },
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(3),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 
 export default function RegisterForm(props) {
+  const classes = useStyles();
+  const [form, setForm] = useState({
+    email: {
+      value: "",
+      error: false
+    },
+    password: {
+      value: "",
+      error: false
+    },
+    confirmPassword: {
+      value: "",
+      error: false
+    }
+  });
 
-    const classes = useStyles();
-    const [form, setForm] = useState({
+  const handleInputChange = event => {
+    event.persist(); // https://fb.me/react-event-pooling
+    const newValue = {
+      value: event.target.value,
+      error: false // validate
+    };
+    setForm(form => ({
+      ...form,
+      [event.target.name]: newValue
+    }));
+  };
+
+  const handleOnClickButton = () => {
+    const formValues = Object.assign({}, form);
+    props.onChange(formValues).then(() => {
+      setForm({
         username: {
-            value: '',
-            error: false
+          value: "",
+          error: false
         },
         password: {
-            value: '',
-            error: false
+          value: "",
+          error: false
         },
         confirmPassword: {
-            value: '',
-            error: false
+          value: "",
+          error: false
         }
+      });
     });
+  };
 
-    const handleInputChange = (event) => {
-        event.persist(); // https://fb.me/react-event-pooling
-        const newValue = {
-            value: event.target.value,
-            error: false, // validate
-        };
-        setForm(form => ({
-            ...form,
-            [event.target.name]: newValue
-        }));
-    }
-
-    const handleOnClickButton = () => {
-        const formValues = Object.assign({}, form);
-        props.onChange(formValues).then(() => {
-            setForm({
-                username: {
-                    value: '',
-                    error: false
-                },
-                password: {
-                    value: '',
-                    error: false
-                },
-                confirmPassword: {
-                    value: '',
-                    error: false
-                }
-            });
-        });
-    }
-
-    return (
-        <div className={classes.form} noValidate>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <TextField
-                        autoComplete="username"
-                        variant="outlined"
-                        required
-                        fullWidth
-                        autoFocus
-                        id="username"
-                        name="username"
-                        label="Username"
-                        onChange={handleInputChange}
-                        value={form.username.value}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        onChange={handleInputChange}
-                        value={form.password.value}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="confirmPassword"
-                        name="confirmPassword"
-                        label="Confirm Password"
-                        type="password"
-                        onChange={handleInputChange}
-                        value={form.confirmPassword.value}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <FormControlLabel
-                        control={<Checkbox value="allowExtraEmails" color="primary" />}
-                        label="I read the terms and conditions."
-                    />
-                </Grid>
-            </Grid>
-            <Button
-                type="button"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-                onClick={handleOnClickButton}
-            >
-                Sign Up
-          </Button>
-        </div>
-    );
+  return (
+    <div className={classes.form} noValidate>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            autoComplete="email"
+            variant="outlined"
+            required
+            fullWidth
+            autoFocus
+            id="email"
+            name="email"
+            label="Email"
+            onChange={handleInputChange}
+            value={form.email.value}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={handleInputChange}
+            value={form.password.value}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="confirmPassword"
+            name="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            onChange={handleInputChange}
+            value={form.confirmPassword.value}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox value="allowExtraEmails" color="primary" />}
+            label="I read the terms and conditions."
+          />
+        </Grid>
+      </Grid>
+      <Button
+        type="button"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+        onClick={handleOnClickButton}
+      >
+        Sign Up
+      </Button>
+    </div>
+  );
 }
